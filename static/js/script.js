4f9c95233ad28f7371d090f1c6859fe01dc3c1ac
jas-sin82 <jasvir198@yahoo.co.in>
2021-08-17 11:36:37 +0000

    Create nav bar close and open menu function

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+/*
+Functions which will open/show the overlay navigation menu
+*/
+
+function openMenu(){
+    document.getElementById("menu_bar").style.width = "100%";
+ }
+ 
+ function closeMenu(){
+    document.getElementById("menu_bar").style.width = "0%";
+ }
