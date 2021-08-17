09126e3867ecf238389df60a217783d4bae1d5b1
jas-sin82 <jasvir198@yahoo.co.in>
2021-08-17 16:08:39 +0000

    create emailjs function

--- static/js/script.js
+++ static/js/script.js
@@ -2,10 +2,55 @@
 Functions which will open/show the overlay navigation menu
 */
 
-function openMenu(){
+function openMenu() {
     document.getElementById("menu_bar").style.width = "100%";
- }
- 
- function closeMenu(){
+}
+
+function closeMenu() {
     document.getElementById("menu_bar").style.width = "0%";
- }
+}
+
+/* 
+Call function to intialise emailjs
+*/
+
+(function () {
+    emailjs.init("user_ogk8J5XmQvETaxv1fZva0");
+})();
+
+function sendMail(contactForm) {
+    emailjs.send("gmail", "template_i3671bf", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "message": contactForm.message.value
+        })
+
+        .then(
+            function () {
+                document.getElementById("contact-form").reset();
+            })
+
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                emailSuccess();
+            },
+            function (error) {
+
+                console.log("FAILED", error);
+                unsuccess();
+
+            }
+        );
+    return false;
+
+}
+
+function emailSuccess() {
+    swal({
+        title: "Great!",
+        text: "Your message has been sent successfully!",
+        icon: "success",
+        button: "ok!",
+    });
+}
